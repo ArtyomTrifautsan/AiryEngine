c89849ef6bd7864c4f268f23af1639695bc9644f
ArtyomTrifautsan <trifautsan01@mail.ru>
2024-07-23 21:23:53 +0500

    Bug has fixed

--- AiryEngineCore/includes/AiryEngineCore/Camera.hpp
+++ AiryEngineCore/includes/AiryEngineCore/Camera.hpp
@@ -17,7 +17,7 @@ namespace AiryEngine {
         Camera(const glm::vec3& position = { 0, 0, 0 },
                const glm::vec3& rotation = { 0, 0, 0 },
                const ProjectionMode projection_mode = ProjectionMode::Perspective);
-        
+
         void set_position(const glm::vec3& position);
         void set_rotation(const glm::vec3& rotation);
         void set_position_rotation(const glm::vec3& position, const glm::vec3& rotation);
@@ -29,8 +29,8 @@ namespace AiryEngine {
         void update_view_matrix();
         void update_projection_matrix();
 
-        glm::vec3& m_position;
-        glm::vec3& m_rotation;
+        glm::vec3 m_position;
+        glm::vec3 m_rotation;
         ProjectionMode m_projection_mode;
         glm::mat4 m_view_matrix;
         glm::mat4 m_projection_matrix;
